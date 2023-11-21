3812af5d1fcb45e89d18e3f9747ac93831032a5e
kae lucas <kaeteodoro@gmail.com>
2023-11-21 09:21:11 -0300

    Changing user repository to a interface

--- smart_wallet_ms/src/main/java/com/br/smart_wallet_ms/repository/UserRepository.java
+++ smart_wallet_ms/src/main/java/com/br/smart_wallet_ms/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.br.smart_wallet_ms.repository;
 
+import com.br.smart_wallet_ms.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<UserModel, Long> {
 
 }
