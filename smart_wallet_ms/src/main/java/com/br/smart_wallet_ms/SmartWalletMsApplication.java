0ce3e8b2e9913cf62343d3fce4c6366d4245eb0d
kae lucas <kaeteodoro@gmail.com>
2023-11-21 08:31:15 -0300

    main

---
+++ smart_wallet_ms/src/main/java/com/br/smart_wallet_ms/SmartWalletMsApplication.java
@@ -0,0 +1,13 @@
+package com.br.smart_wallet_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWalletMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartWalletMsApplication.class, args);
+    }
+
+}
---
+++ smart_wallet_ms/src/test/java/com/br/smart_wallet_ms/SmartWalletMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.smart_wallet_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartWalletMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
