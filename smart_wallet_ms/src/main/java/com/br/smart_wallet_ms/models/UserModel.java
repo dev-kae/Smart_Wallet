12a5fe1a2a79df3f2108c5068c8e1b91a344175f
kae lucas <kaeteodoro@gmail.com>
2023-11-21 09:17:50 -0300

    setup usermodel

--- smart_wallet_ms/src/main/java/com/br/smart_wallet_ms/models/UserModel.java
+++ smart_wallet_ms/src/main/java/com/br/smart_wallet_ms/models/UserModel.java
@@ -1,19 +1,34 @@
 package com.br.smart_wallet_ms.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.io.Serializable;
 import java.util.UUID;
 
 @Entity
+@Table(name = "User")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserModel implements Serializable {
     private final Long serialVersion = 1L;
 
     @Id
-    UUID id;
-    String name;
-    String password;
-    String email;
-    String profilePicture;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "profilePicture")
+    private String profilePicture;
 }
