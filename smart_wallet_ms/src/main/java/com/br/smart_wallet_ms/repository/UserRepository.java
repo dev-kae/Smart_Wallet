c227b6795373e3cfb6650fec4e69b11a9e50bcfe
Danillo Leal <tmdanillomartins@gmail.com>
2023-12-02 13:17:31 -0300

    removing unused imports

--- smart_wallet_ms/src/main/java/com/br/smart_wallet_ms/repository/UserRepository.java
+++ smart_wallet_ms/src/main/java/com/br/smart_wallet_ms/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.br.smart_wallet_ms.repository;
 
 import com.br.smart_wallet_ms.models.UserModel;
-import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
